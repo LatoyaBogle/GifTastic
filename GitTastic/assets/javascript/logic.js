72c8c079859411c05cfbb5490eed4c4158af352b
LatoyaBogle <41763275+LatoyaBogle@users.noreply.github.com>
2018-09-13 19:53:28 -0400

    Update logic.js
    adding s to http

--- GitTastic/assets/javascript/logic.js
+++ GitTastic/assets/javascript/logic.js
@@ -21,7 +21,7 @@ function populateButtons(searchArray, classtoAdd, areaToAddTo){
 $(document).on('click', '.searchButton', function(){
     //$('#searches').empty();
     var type =$(this).data('type');
-    var queryURL = 'http://api.giphy.com/v1/gifs/search?q='+type+'&api_key=Twc7CqTN5a9QH3EuZbDWcztXLH7rdYM7&limit=10';
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?q='+type+'&api_key=Twc7CqTN5a9QH3EuZbDWcztXLH7rdYM7&limit=10';
     $.ajax({url:queryURL,method:'GET'})
         .done(function(response){
             for(var i=0; i<response.data.length;i++){
